fd8cb3498ede99fd2f73e98411798a47238fceac
Sergey <podlesskii@hotmail.com>
2026-04-22 15:59:19 +0200

    Switch image model from gpt-image-1.5 to gpt-image-2

--- main.py
+++ main.py
@@ -1141,7 +1141,7 @@ async def _generate_image(
 
     async def _try(p: str):
         response = await client.images.generate(
-            model="gpt-image-1.5",
+            model="gpt-image-2",
             prompt=p,
             n=1,
             size="1024x1024",
